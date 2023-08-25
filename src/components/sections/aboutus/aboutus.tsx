83885fa0004e9036c5a428c520e62029085ef1e8
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2023-08-25 22:01:13 +0530

    Commit

--- src/components/sections/aboutus/aboutus.tsx
+++ src/components/sections/aboutus/aboutus.tsx
@@ -4,62 +4,60 @@ import React from "react";
 
 export default function Aboutus() {
     return (
-        <div className="bg-gradient2 flex flex-col items-center justify-center h-full py-10 px-14">
-                <h1 className="flex items-center justify-center text-4xl md:text-5xl lg:text-6xl text-white font-sub-heading py-10">
-                    About Us
-                </h1>
-                <div className="h-full bg-gradient rounded-2xl p-10">
-                    <div className="text-center">
-                        <p className="text-gray-200 mt-2"> {/* Reduced mt-4 to mt-2 */}
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Nullam eget pretium nunc, a vehicula neque. Fusce eget
-                            odio massa. Vivamus consequat, arcu vel efficitur auctor,
-                            ex ex euismod elit.
-                        </p>
+        <div className="bg-gradient2 flex flex-col items-center justify-center h-full py-10 px-24">
+            <h1 className="flex items-center justify-center text-4xl md:text-5xl lg:text-6xl text-white font-sub-heading py-10">
+                About Us
+            </h1>
+            <div className="h-full bg-gradient rounded-2xl p-10 m-2 text-center">
+                <p className="text-gray-200 mt-2"> {/* Reduced mt-4 to mt-2 */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam eget pretium nunc, a vehicula neque. Fusce eget
+                    odio massa. Vivamus consequat, arcu vel efficitur auctor,
+                    ex ex euismod elit.
+                </p>
 
-                        <p className="text-gray-200 mt-4">
-                            Mauris eu libero nec justo facilisis tincidunt.
-                            Suspendisse vel tortor facilisis, aliquet lorem eu,
-                            vehicula nisi. Vivamus ut fringilla nunc. Nulla facilisi.
-                            Nunc vehicula sem vel ante laoreet, in auctor dolor
-                            fringilla.
-                        </p>
-                        <p className="text-gray-200 mt-4">
-                            Sed in ante id est hendrerit mattis a quis ex.
-                            Suspendisse eget justo vel felis vehicula egestas vel
-                            at dolor. Duis fermentum varius libero, ut
-                            consectetur elit venenatis nec.
-                        </p>
-                        <p className="text-gray-200 mt-4">
-                            Proin viverra libero eget arcu feugiat, a tristique
-                            justo sollicitudin. Etiam non lectus eu nunc egestas
-                            venenatis. Aliquam erat volutpat. Duis viverra quam
-                            nec urna feugiat, a tempus justo hendrerit.
-                        </p>
-                        <p className="text-gray-200 mt-4">
+                <p className="text-gray-200 mt-4">
+                    Mauris eu libero nec justo facilisis tincidunt.
+                    Suspendisse vel tortor facilisis, aliquet lorem eu,
+                    vehicula nisi. Vivamus ut fringilla nunc. Nulla facilisi.
+                    Nunc vehicula sem vel ante laoreet, in auctor dolor
+                    fringilla.
+                </p>
+                <p className="text-gray-200 mt-4">
+                    Sed in ante id est hendrerit mattis a quis ex.
+                    Suspendisse eget justo vel felis vehicula egestas vel
+                    at dolor. Duis fermentum varius libero, ut
+                    consectetur elit venenatis nec.
+                </p>
+                <p className="text-gray-200 mt-4">
+                    Proin viverra libero eget arcu feugiat, a tristique
+                    justo sollicitudin. Etiam non lectus eu nunc egestas
+                    venenatis. Aliquam erat volutpat. Duis viverra quam
+                    nec urna feugiat, a tempus justo hendrerit.
+                </p>
+                <p className="text-gray-200 mt-4">
 
-                            Fusce a lacinia ipsum, eu facilisis purus. Sed
-                            sollicitudin ultricies ex, non tristique ipsum luctus
-                            id. Maecenas sit amet turpis nec arcu sodales sodales.
-                            Curabitur tristique nisi eget elit blandit, a
-                            sollicitudin massa fermentum.
-                        </p>
-                        <p className="text-gray-200 mt-4">
+                    Fusce a lacinia ipsum, eu facilisis purus. Sed
+                    sollicitudin ultricies ex, non tristique ipsum luctus
+                    id. Maecenas sit amet turpis nec arcu sodales sodales.
+                    Curabitur tristique nisi eget elit blandit, a
+                    sollicitudin massa fermentum.
+                </p>
+                <p className="text-gray-200 mt-4">
 
-                            Vestibulum bibendum bibendum libero, a accumsan sem
-                            volutpat id. In hac habitasse platea dictumst.
-                            Suspendisse sit amet augue ac quam iaculis tempus.
-                            Etiam a arcu sed odio dignissim dictum.
-                        </p>
-                        <p className="text-gray-200 mt-4">
+                    Vestibulum bibendum bibendum libero, a accumsan sem
+                    volutpat id. In hac habitasse platea dictumst.
+                    Suspendisse sit amet augue ac quam iaculis tempus.
+                    Etiam a arcu sed odio dignissim dictum.
+                </p>
+                <p className="text-gray-200 mt-4">
 
-                            Nunc malesuada, metus nec fermentum finibus, erat elit
-                            fermentum odio, ac volutpat justo augue nec felis.
-                            Quisque vel sodales nisl, et condimentum nulla. Integer
-                            aliquam feugiat lectus, eu varius leo vehicula vel.
-                        </p>
-                    </div>
-                </div>
+                    Nunc malesuada, metus nec fermentum finibus, erat elit
+                    fermentum odio, ac volutpat justo augue nec felis.
+                    Quisque vel sodales nisl, et condimentum nulla. Integer
+                    aliquam feugiat lectus, eu varius leo vehicula vel.
+                </p>
+            </div>
         </div>
     );
 }
--- src/components/sections/featuredon/featuredon.tsx
+++ src/components/sections/featuredon/featuredon.tsx
@@ -3,47 +3,50 @@ import fastcompany from "../../Images/image-5@2x.png";
 import techcrunch from "../../Images/image-6@2x.png";
 import mit from "../../Images/image-7@2x.png";
 import forbes from "../../Images/image-8@2x.png";
+import "./featuredon.css";
 
 export default function Featuredon() {
     return (
-        <>
-            <div className={"h-screen flex flex-col items-center justify-center relative"}>
-                <b className="text-xl tracking-[0.1em] leading-[27.5px] uppercase inline-block font-sub-heading text-white p-2.5">
-                    Featured on
-                </b>
-                <div className="w-970 h-102 flex-shrink-0 rounded-3xl bg-bg-rgba bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
-                    <div className={"flex items-center"}>
+        <div className="h-screen flex flex-col items-center justify-center relative">
+            <b className="text-xl tracking-[0.1em] leading-[27.5px] uppercase inline-block font-sub-heading text-white p-10">
+                Featured on
+            </b>
+            <div className="logos w-2/3 h-102 flex-shrink-0 rounded-3xl bg-bg-rgba bg-clip-padding backdrop-filter backdrop-blur-sm p-2">
+                <div className="flex items-center justify-between">
+                    <div className="m-2 px-2">
                         <Image
                             src={techcrunch}
-                            alt={"fastcompany"}
+                            alt="fastcompany"
                             width={196.332}
                             height={60.464}
-                            className="m-2"
                         />
+                    </div>
+                    <div className="m-2 px-2">
                         <Image
                             src={fastcompany}
-                            alt={"fastcompany"}
+                            alt="fastcompany"
                             width={175.556}
                             height={22.674}
-                            className=" m-2"
                         />
+                    </div>
+                    <div className="m-2 px-2">
                         <Image
                             src={mit}
-                            alt={"fastcompany"}
+                            alt="fastcompany"
                             width={70.638}
                             height={32.391}
-                            className="m-2"
                         />
+                    </div>
+                    <div className="m-2 px-2">
                         <Image
                             src={forbes}
-                            alt={"fastcompany"}
+                            alt="fastcompany"
                             width={119.461}
                             height={32.391}
-                            className="m-2"
                         />
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
