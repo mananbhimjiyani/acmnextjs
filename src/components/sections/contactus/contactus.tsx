62860735e0ba1af36d17efaad9231a5f50ff86cb
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2023-09-10 12:29:16 +0530

    Manan's Commit

--- src/components/sections/contactus/contactus.tsx
+++ src/components/sections/contactus/contactus.tsx
@@ -1,4 +1,14 @@
 import React from "react";
 import "./contactus.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+function ContactUs() {
+    return (
+        <>
+            <footer className="h-screen">
 
+            </footer>
+        </>
+    )
+}
+
+export default ContactUs;
