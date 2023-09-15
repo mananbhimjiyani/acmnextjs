ebd9bd475d3c6f544406159c666a30e379393426
Malvericus <malvikabhadoriya@gmail.com>
2023-09-15 09:09:53 +0530

    Malvika's 2nd Commit

--- src/components/sections/footer/footer.tsx
+++ src/components/sections/footer/footer.tsx
@@ -1,21 +1,20 @@
+import "./footer.css";
+
 export default function Footer() {
     return (
         <>
-            <footer className={"h-max"}>
-                <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <footer className="footer background-image">
+                <div className="mx-auto max-w-screen-xl p-4 py-6 lg:py-8 rounded-2xl text-center">
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
-                            <a href="https://flowbite.com/" className="flex items-center">
-                                <img src="http://localhost:3000/_next/image?url=https%3A%2F%2Fimagizer.imageshack.com%2Fimg923%2F4372%2F6Pl6Bz.png&w=256&q=75" className="h-auto mr-auto"
-                                     alt="FlowBite Logo"/>
-                                <span
-                                    className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                            </a>
+                            <img src="http://localhost:3000/_next/image?url=https%3A%2F%2Fimagizer.imageshack.com%2Fimg923%2F4372%2F6Pl6Bz.png&w=256&q=75" className="h-52 mx-auto"
+                            alt="ACM-SIT Logo"/>
+
                         </div>
-                        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 py-8">
                             <div>
                                 <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Resources</h2>
-                                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                <ul className="text-gray-500 dark:text-gray-200 font-medium">
                                     <li className="mb-4">
                                         <a href="https://www.acm.org//" className="hover:underline">ACM</a>
                                     </li>
@@ -27,7 +26,7 @@ export default function Footer() {
                             <div>
                                 <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Follow
                                     us</h2>
-                                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                <ul className="text-gray-500 dark:text-gray-200 font-medium">
                                     <li className="mb-4">
                                         <a href="https://github.com/themesberg/flowbite"
                                            className="hover:underline ">Github</a>
@@ -39,7 +38,7 @@ export default function Footer() {
                             </div>
                             <div>
                                 <h2 className="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Legal</h2>
-                                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                <ul className="text-gray-500 dark:text-gray-200 font-medium">
                                     <li className="mb-4">
                                         <a href="#" className="hover:underline">Privacy Policy</a>
                                     </li>
@@ -50,13 +49,13 @@ export default function Footer() {
                             </div>
                         </div>
                     </div>
-                    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+                    <hr className="my-6 border-gray-200 sm:mx-auto light:border-gray-200 lg:my-8"/>
                     <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-300">© 2023 <a
               href="https://acm.org/" className="hover:underline">SIT-ACM™</a>. All Rights Reserved.
           </span>
                         <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-                            <a href="#" className="text-gray-500 hover:text-white dark:hover:text-white">
+                            <a href="#" className="text-gray-300 hover:text-white dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="currentColor" viewBox="0 0 8 19">
                                     <path fill-rule="evenodd"
@@ -65,7 +64,7 @@ export default function Footer() {
                                 </svg>
                                 <span className="sr-only">Facebook page</span>
                             </a>
-                            <a href="#" className="text-gray-500 hover:text-white dark:hover:text-white">
+                            <a href="#" className="text-gray-300 hover:text-white dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="currentColor" viewBox="0 0 21 16">
                                     <path
@@ -73,7 +72,7 @@ export default function Footer() {
                                 </svg>
                                 <span className="sr-only">Discord community</span>
                             </a>
-                            <a href="#" className="text-gray-500 hover:text-white dark:hover:text-white">
+                            <a href="#" className="text-gray-300 hover:text-white dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="currentColor" viewBox="0 0 20 17">
                                     <path fill-rule="evenodd"
@@ -82,7 +81,7 @@ export default function Footer() {
                                 </svg>
                                 <span className="sr-only">Twitter page</span>
                             </a>
-                            <a href="#" className="text-gray-500 hover:text-white dark:hover:text-white">
+                            <a href="#" className="text-gray-300 hover:text-white dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd"
@@ -91,7 +90,7 @@ export default function Footer() {
                                 </svg>
                                 <span className="sr-only">GitHub account</span>
                             </a>
-                            <a href="#" className="text-gray-500 hover:text-white dark:hover:text-white">
+                            <a href="#" className="text-gray-300 hover:text-white dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd"
--- src/components/sections/navbar/navbar.tsx
+++ src/components/sections/navbar/navbar.tsx
@@ -1,80 +1,138 @@
-import { Link } from 'react-scroll';
+import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 import Image from "next/image";
 
 export default function Navbar() {
-    const navHeight = 80;
-    return (
-        <nav className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 bg-black bg-opacity-75">
-            <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-                <a className="text-white text-2xl font-bold hover:text-white hover:cursor-pointer">
-                    <Image src={"https://imagizer.imageshack.com/img923/4372/6Pl6Bz.png"} alt={"Logo"} width={"80"} height={"80"} />
-                </a>
-                <div className="space-x-4">
-                    <Link
-                        to="hero"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        to="featuredon"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        Featured
-                    </Link>
-                    <Link
-                        to="gallery"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        Gallery
-                    </Link>
-                    <Link
-                        to="aboutus"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        About Us
-                    </Link>
-                    <Link
-                        to="events"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        Events
-                    </Link>
-                    <Link
-                        to="team"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        Team
-                    </Link>
-                    <Link
-                        to="footer"
-                        smooth={true}
-                        duration={500}
-                        className="text-white hover:cursor-pointer"
-                        offset={-navHeight}
-                    >
-                        Contact Us
-                    </Link>
-                </div>
-            </div>
-        </nav>
-    );
+  const [isVisible, setIsVisible] = useState(true);
+  const navHeight = 80;
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const isScrolledDown = prevScrollPos < currentScrollPos;
+
+      setIsVisible(!isScrolledDown);
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed top-0 left-0 right-0 z-10 bg-black shadow-md transition-transform ${
+        isVisible ? "" : "transform -translate-y-full"
+      }`}
+    >
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a className="flex items-center">
+          <Image
+            src={"https://imagizer.imageshack.com/img923/4372/6Pl6Bz.png"}
+            alt={"Logo"}
+            width={"70"}
+            height={"10"}
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            SIT-ACM
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="hero"
+                style={{ color: "#739BD0" }}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="featuredon"
+                style={{ color: "#739BD0" }}
+              >
+                Featured On
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="gallery"
+                style={{ color: "#739BD0" }}
+              >
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="aboutus"
+                style={{ color: "#739BD0" }}
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="events"
+                style={{ color: "#739BD0" }}
+              >
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="team"
+                style={{ color: "#739BD0" }}
+              >
+                Team
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block py-2 pl-3 pr-4 rounded md:p-0 hover:cursor-pointer"
+                to="footer"
+                style={{ color: "#739BD0" }}
+              >
+                Footer
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
